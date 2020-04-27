84e58052145bedb6fb309a406db2465edf34680e
root <kevin.sandermann@gmail.com>
2020-04-27 12:19:20 +0000

    update

--- vnet.tf
+++ vnet.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "main" {
   name                = module.naming-hub.vnet_names["main"]
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  address_space       = concat(var.subnet_shared_services_cidr, var.subnet_gateway_cidr, var.subnet_dmz_cidr)
+  address_space       = concat([var.subnet_shared_services_cidr], [var.subnet_gateway_cidr], [var.subnet_dmz_cidr])
   dns_servers         = var.vnet_main_dns_servers
 
   ddos_protection_plan {
