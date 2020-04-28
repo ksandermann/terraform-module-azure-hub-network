0f25ac3fe92e7e953203078feb282f62343c23c5
root <kevin.sandermann@gmail.com>
2020-04-28 08:36:00 +0000

    fw

--- firewall.tf
+++ firewall.tf
@@ -1,7 +1,7 @@
 resource "azurerm_firewall" "dmz" {
   name                = module.naming-hub.azure_firewall_names["dmz"]
-  location            = data.azurerm_resource_group.dmz.location
-  resource_group_name = data.azurerm_resource_group.dmz.name
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
   zones               = var.firewall_dmz_zones
   tags                = local.tags_firewall_dmz
 
@@ -14,8 +14,8 @@ resource "azurerm_firewall" "dmz" {
 
 resource "azurerm_public_ip" "azure_firewall_dmz" {
   name                = format("%s%s", module.naming-hub.azure_firewall_names["dmz"], "PUB001")
-  location            = data.azurerm_resource_group.dmz.location
-  resource_group_name = data.azurerm_resource_group.dmz.name
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
   allocation_method   = "Static"
   sku                 = "Standard"
   tags                = local.tags_firewall_dmz
